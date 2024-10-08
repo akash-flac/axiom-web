bd51a697b2cdb338f56a30b31512ede72a04a09d
monk <akashmauryame@gmail.com>
2024-10-08 14:02:57 +0530

    adding the project to github

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Home from "./Home.jsx";
+import About from "./About.jsx"
+import Departments from "./thoughts.jsx"
+import Explore from "./explore.jsx";
+
+export {
+    Explore,
+    About,
+    Navbar,
+    Footer,
+    Testimonials,
+    Home,
+    Departments,
+};
